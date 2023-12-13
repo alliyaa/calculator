2ddedc99929787c85982a1d1fc70b912eea1483e
alliyaa <alliyaahmad#@gmail.com>
2023-12-13 08:52:21 +0300

    created the basic display for the numbers on calculator screen

--- script.js
+++ script.js
@@ -56,75 +56,90 @@ else {
 operaterx('-', 8, 1);
 
 let appendinput;
+let appendoperater;
+
 const btn1= document.querySelector('#one');
 btn1.addEventListener('click',function()
 {
 appendinput=1;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
 const btn2= document.querySelector('#two');
 btn2.addEventListener('click',function()
 {
 appendinput=2;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
 const btn3= document.querySelector('#three');
 btn3.addEventListener('click',function()
 {
 appendinput=3;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
 const btn4= document.querySelector('#four');
 btn4.addEventListener('click',function()
 {
 appendinput=4;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
 const btn5= document.querySelector('#five');
 btn5.addEventListener('click',function()
 {
 appendinput=5;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
 const btn6= document.querySelector('#six');
 btn6.addEventListener('click',function()
 {
 appendinput=6;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
 const btn7= document.querySelector('#seven');
 btn7.addEventListener('click',function()
 {
 appendinput=7;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
 const btn8= document.querySelector('#eight');
 btn8.addEventListener('click',function()
 {
 appendinput=8;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
 const btn9= document.querySelector('#nine');
 btn9.addEventListener('click',function()
 {
 appendinput=9;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
 const btn0= document.querySelector('#zero');
 btn0.addEventListener('click',function()
 {
 appendinput=0;
-console.log(appendinput);
+displayval(appendinput);
 }
 );
+let dispv;
+
+let sd= document.getElementById("sd");
+//sd.innerHTML+="<p> ji </p>";
+
+function displayval(appendinput)
+{
+    sd.innerHTML= ' ';
+    console.log(appendinput);
+    dispv=appendinput;
+    sd.innerHTML+="<h4>" + dispv + "</h4>";
+    return dispv;
+}
 
 
